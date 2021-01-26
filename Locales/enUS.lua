35e392438fa350b147ee2eeb0a476a03b8558603
Michael Peterson <33461139+mtp1032@users.noreply.github.com>
2021-01-26 15:12:04 -0700

    Updated enUS.lua
    Added new entries and deleted a number of unsed entries.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -6,7 +6,6 @@
 
 local _, VisualThreat = ...
 VisualThreat.enUS = {}
--- en = VisualThreat.enUS
 
 local L = setmetatable({}, { __index = function(t, k)
 	local v = tostring(k)
@@ -24,19 +23,16 @@ if LOCALE == "enUS" then
 	L["ADDON_NAME"]							= "VisualThreat - A Graphical Threat Display"
 	L["VERSION"]							= ", V0.1 (ShadowLands)"
 	L["LOADED"]								= "loaded"
-	L["ADDON_AND_VERSION"] 					= sprintf("%s %s", L["ADDON_NAME"], L["VERSION"] )
 	L["ADDON_LOADED_MESSAGE"] 				= sprintf("%s %s %s", L["ADDON_NAME"], L["LOADED"], L["VERSION"] )
-
-	L["ERROR_MSG_FRAME_TITLE"]			= "ERROR"
 	L["USER_MSG_FRAME"]					= sprintf("%s %s", L["ADDON_AND_VERSION"], "User Messages")
-	L["HELP_FRAME_TITLE"]				= sprintf("HELP - %s", L["ADDON_AND_VERSION"])
-	L["ADDON_LOADED_MSG"]				= sprintf("%s loaded (Use /VT for help).", L["ADDON_AND_VERSION"])
 
 	-- Generic Error Message
-    L["ERROR_MSG"]            	= "[ERROR] %s"	
 
-	L["ARG_NIL"]				= "INVALID: Parameter Was nil."
-	L["ARG_INVALID_VALUE"]		= "INVALID: Parameter Not In Range."
-	L["ARG_INVALID_TYPE"]		= "INVALID: Parameter Was Wrong Type."
+	L["ARG_NIL"]				= "Value Was nil"
+	L["ARG_MISSING"]			= "Parameter missing"
+	L["ARG_INVALID_VALUE"]		= "Value Not In Range"
+	L["ARG_UNEXPECTED_VALUE"]	= "Value Unexpected or Not In Range"
+	L["ARG_UNEQUAL_VALUES"]		= "Unequal Values"
+	L["ARG_INVALID_TYPE"]		= "Variable was type %s, expected %s"
 	return 
 end
